66ac203b017d64e01095257260dd4acc02a5fa93
Andrew Thorpe <pyroka@gmail.com>
2015-12-14 23:53:11 +0000

    Fixed up the tests to my new style. Imported and fixed up the score function

--- Tests/roll_test.rb
+++ Tests/roll_test.rb
@@ -5,49 +5,47 @@ module RubyGreed
   class RollTest < Test::Unit::TestCase
 
     def test_initialize_with_values_stores_values
-      roll = Roll.new([1, 2, 3])
-
-      assert_equal([1, 2, 3], roll.values)
+      assert_equal([1, 2, 3], Roll.new([1, 2, 3]).values)
     end
 
     def test_score_of_an_empty_list_is_zero
-      assert_equal 0, score([])
+      assert_equal(0, Roll.new([]).score)
     end
 
     def test_score_of_a_single_roll_of_5_is_50
-      assert_equal 50, score([5])
+      assert_equal(50, Roll.new([5]).score)
     end
 
     def test_score_of_a_single_roll_of_1_is_100
-      assert_equal 100, score([1])
+      assert_equal(100, Roll.new([1]).score)
     end
 
     def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
-      assert_equal 300, score([1, 5, 5, 1])
+      assert_equal(300, Roll.new([1, 5, 5, 1]).score)
     end
 
     def test_score_of_single_2s_3s_4s_and_6s_are_zero
-      assert_equal 0, score([2, 3, 4, 6])
+      assert_equal(0, Roll.new([2, 3, 4, 6]).score)
     end
 
     def test_score_of_a_triple_1_is_1000
-      assert_equal 1000, score([1, 1, 1])
+      assert_equal(1000, Roll.new([1, 1, 1]).score)
     end
 
     def test_score_of_other_triples_is_100x
-      assert_equal 200, score([2, 2, 2])
-      assert_equal 300, score([3, 3, 3])
-      assert_equal 400, score([4, 4, 4])
-      assert_equal 500, score([5, 5, 5])
-      assert_equal 600, score([6, 6, 6])
+      assert_equal(200, Roll.new([2, 2, 2]).score)
+      assert_equal(300, Roll.new([3, 3, 3]).score)
+      assert_equal(400, Roll.new([4, 4, 4]).score)
+      assert_equal(500, Roll.new([5, 5, 5]).score)
+      assert_equal(600, Roll.new([6, 6, 6]).score)
     end
 
     def test_score_of_mixed_is_sum
-      assert_equal 250, score([2, 5, 2, 2, 3])
-      assert_equal 550, score([5, 5, 5, 5])
-      assert_equal 1100, score([1, 1, 1, 1])
-      assert_equal 1200, score([1, 1, 1, 1, 1])
-      assert_equal 1150, score([1, 1, 1, 5, 1])
+      assert_equal(250, Roll.new([2, 5, 2, 2, 3]).score)
+      assert_equal(550, Roll.new([5, 5, 5, 5]).score)
+      assert_equal(1100, Roll.new([1, 1, 1, 1]).score)
+      assert_equal(1200, Roll.new([1, 1, 1, 1, 1]).score)
+      assert_equal(1150, Roll.new([1, 1, 1, 5, 1]).score)
     end
 
   end
--- roll.rb
+++ roll.rb
@@ -5,5 +5,27 @@ module RubyGreed
     def initialize(values)
       @values = values
     end
+
+    def score
+      rollToCounts = Hash.new(0)
+      @values.each { |value| rollToCounts[value] += 1 }
+
+      score = score_triple(rollToCounts, 1, 1000)
+      for die in (2..6)
+        score += score_triple(rollToCounts, die, die * 100)
+      end
+      score += (rollToCounts[1] * 100)
+      score + (rollToCounts[5] * 50)
+    end
+
+    private
+
+    def score_triple(rollToCounts, index, score)
+      if rollToCounts[index] >= 3
+        rollToCounts[index] -= 3
+        return score
+      end
+      return 0
+    end
   end
 end
