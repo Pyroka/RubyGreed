97af49f04d61b7c698093b87d821fbc6a2fbbad7
Andrew Thorpe <pyroka@gmail.com>
2015-12-15 00:47:08 +0000

    Some refactoring

--- roll.rb
+++ roll.rb
@@ -1,26 +1,32 @@
 module RubyGreed
   class Roll
+    attr_reader :score
+    attr_reader :num_non_scoring_dice
+
     def initialize(values)
-      @roll_to_counts = Hash.new(0)
-      values.each { |value| @roll_to_counts[value] += 1 }
-    end
+      roll_to_counts = Hash.new(0)
+      values.each { |value| roll_to_counts[value] += 1 }
 
-    def score
-      score = score_triple(@roll_to_counts, 1, 1000)
-      (2..6).each { |die| score += score_triple(@roll_to_counts, die, die * 100) }
-      score += (@roll_to_counts[1] * 100)
-      score + (@roll_to_counts[5] * 50)
-    end
-
-    def num_non_scoring_dice
-      [2, 3, 4, 6].inject(0) { |sum, die| sum + @roll_to_counts[die] }
+      @score = calculate_score(roll_to_counts)
+      @num_non_scoring_dice = calculate_num_non_scoring_dice(roll_to_counts)
     end
 
     private
 
-    def score_triple(rollToCounts, index, score)
-      if rollToCounts[index] >= 3
-        rollToCounts[index] -= 3
+    def calculate_score(roll_to_counts)
+      score = score_triple(roll_to_counts, 1, 1000)
+      (2..6).each { |die| score += score_triple(roll_to_counts, die, die * 100) }
+      score += (roll_to_counts[1] * 100)
+      score + (roll_to_counts[5] * 50)
+    end
+
+    def calculate_num_non_scoring_dice(roll_to_counts)
+      [2, 3, 4, 6].inject(0) { |sum, die| sum + roll_to_counts[die] }
+    end
+
+    def score_triple(roll_to_counts, index, score)
+      if roll_to_counts[index] >= 3
+        roll_to_counts[index] -= 3
         score
       else
         0
